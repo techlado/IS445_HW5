b5278490f873129f90e6ce2e7d839640905734b7
Danielle Gonzalez <15gonzalezdanielle@gmail.com>
2024-03-14 11:41:29 -0700

    First draft

---
+++ js/ex6.js
@@ -0,0 +1,18 @@
+const words = [{
+  term: "Procrastination",
+  definition: "Avoidance of doing a task that needs to be accomplished"
+}, {
+  term: "Tautology",
+  definition: "logical argument constructed in such a way that it is logically irrefutable"
+}, {
+  term: "Oxymoron",
+  definition: "figure of speech that juxtaposes elements that appear to be contradictory"
+}];
+
+const dictFunc() => {
+  const newel = document.createElement("dl");
+  newel.id = "term";
+  newel.textContent = 
+};
+
+words.forEach(dictFunc);
