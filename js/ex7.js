a278d7b70dafba8fa9d0444a3bf6db60688aa76a
Danielle Gonzalez <15gonzalezdanielle@gmail.com>
2024-03-14 15:41:25 -0700

    Update

--- js/ex7.js
+++ js/ex7.js
@@ -1,12 +1,16 @@
-let userInput = prompt("Please enter a number: ");
-let num1 = Number.parseInt(userInput);
+let userInput1 = prompt("Please enter a number: ");
+let num1 = parseInt(userInput1);
 
-userInput = prompt("Please enter a number: ");
-let num2 = Number.parseInt(userInput);
+userInput2 = prompt("Please enter a number: ");
+let num2 = parseInt(userInput2);
 
-if (isNaN(num1) || isNaN(num2)) {
-  document.getElementById("content").innerHTML = '<span style="color:red; font-weight:bold;">Error: Invalid input</span>';
-} else {
+if (isNaN(num1) || isNaN(num2) || !Number.isInteger(userInput1) || !Number.isInteger(userInput2)) {
+  document.getElementById("content").innerHTML = '<span style="color:red; font-weight:bold;">' + 'Error!' + '</span>';
+  
+  document.getElementById("content").innerHTML += '<span>' + ' You must enter integers. You entered "' + userInput1 + '" and "' + userInput2 + '". Try again.' + '</span>';
+  
+} 
+else {
   var sum = num1 + num2;
   document.getElementById("content").innerHTML = 'Sum of ' + num1 + ' and ' + num2 + ' is: <span style="color:red; font-weight:bold;">' + sum + '</span>';
 }
