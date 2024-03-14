7025b727949d58883953bc0655d71560770e46db
Danielle Gonzalez <15gonzalezdanielle@gmail.com>
2024-03-14 15:19:17 -0700

    update

--- js/ex7.js
+++ js/ex7.js
@@ -1,12 +1,14 @@
-let userInput = prompt("Please enter a number between 1 and 100: ");
+let userInput = prompt("Please enter a number: ");
+let num1 = Number.parseInt(userInput);
 
-let number = Number.parseInt(userInput);
+userInput = prompt("Please enter a number: ");
+let num2 = Number.parseInt(userInput);
 
-if (!Number.isNaN(number) && Number.isInteger(number) >= 1 && number <= 100)
-{
-  console.log("Thank you! You entered " + number + ", a valid number.")
+if (isNaN(num1) || isNaN(num2)) {
+  document.getElementById("content").innerHTML = '<span style="color:red; font-weight:bold;">Error: Invalid input</span>';
 } else {
-  console.log("Sorry, " + userInput + " is not a valid entry.")
+  var sum = num1 + num2;
+  document.getElementById("content").innerHTML = 'Sum of ' + num1 + ' and ' + num2 + ' is: <span style="color:red; font-weight:bold;">' + sum + '</span>';
 }
 
 console.log("test");
